1e85cde8edfc6000fabc6528a7fa4f0fef0e62b1
erwan <erwan.cadobts@gmail.com>
2021-01-31 18:04:27 +0100

    FEAT: ADD nouveautés

--- src/router/index.js
+++ src/router/index.js
@@ -11,23 +11,21 @@ const routes = [
     component: Home
   },
   {
-    path: '/movie/:id',
+    path: '/movie/:id(\\d+)',
     name: 'Movie-id',
     component: () => import('../views/Movie.vue')
   },
   {
     path: '/about',
     name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import('../views/About.vue')
   },
   {
     path: "/add",
     name: "Add",
     component: () => import('../views/Add.vue')
-  }
+  },
+
 ]
 
 const router = new VueRouter({
